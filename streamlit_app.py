3640bc986659f9aaea2bbe457b4630cd4fc4e8db
c-cosmin-d <chirazicosmin2002@gmail.com>
2026-03-04 15:47:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+# st.stop()
 
 name_on_order = st.text_input('Name on smoothie:')
 st.write(f"The name on the smoothie is: {name_on_order}")
